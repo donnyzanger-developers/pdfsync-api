784f5e9543ef2b7be371cf153c659186b227755b
JamesSchiller <schillerj78@gmail.com>
2020-06-29 06:46:52 -0400

    moved get files to conversion api functions

--- routes/pdf.js
+++ routes/pdf.js
@@ -20,9 +20,12 @@ async function x(req, res) {
 async function imageToPdf(req, res) {
     try {
         if (req.files.length > 0 && (req.files[0].originalname.endsWith('.jpg') || req.files[0].originalname.endsWith('.jpeg'))) {
-            pdf.imageToPdf(req.files[0])
+            await pdf.imageToPdf(req.files[0])
+            await res.download('./users/1/files/sample.pdf')
+        } else {
+            res.status(400).send()
+            // res.status(200).send()
         }
-        res.status(200).send()
     } catch(err) {
         logger.info(err)
         res.status(500).send()
@@ -33,21 +36,24 @@ async function pdfToImage(req, res) {
     try {
         if (req.files.length > 0  && (req.files[0].originalname.endsWith('.pdf'))) {
             await pdf.pdfToImage(req.files[0])
+            await res.download('./users/1/files/te_1.jpeg')
+        } else {
+            res.status(400).send()
+            // res.status(200).send()
         }
-        res.status(200).send()
     } catch(err) {
         logger.info(err)
         res.status(500).send()
     }
 }
 
-async function getPdf(req, res) {
-    await res.download('./users/1/files/sample.pdf')
-}
+// async function getPdf(req, res) {
+//     await res.download('./users/1/files/sample.pdf')
+// }
 
-async function getImage(req, res) {
-    await res.download('./users/1/files/te_1.jpeg')
-}
+// async function getImage(req, res) {
+//     await res.download('./users/1/files/te_1.jpeg')
+// }
 
 // router.post('/x', webhookCheck.checkAccess, x);
 router.post('/image_to_pdf', webhookCheck.checkAccess, imageToPdf);
